e835296bbdd0e938c137b711929eb5cbe2130549
Gabriel Sancho <gabriel.sancho13@gmail.com>
2020-07-19 19:19:44 -0300

    refact: debug string changes

--- lib/http/interceptors/logging_interceptor.dart
+++ lib/http/interceptors/logging_interceptor.dart
@@ -5,28 +5,28 @@ class LoggingInterceptor extends Interceptor{
 
   @override
   Future<dynamic> onRequest(RequestOptions options) async {
-    debugPrint('Request');
-    debugPrint('url: ${options.uri}');
-    debugPrint('headers: ${options.headers}');
-    debugPrint('body: ${options.data}');
+    debugPrint('->Request');
+    debugPrint('->url: ${options.uri}');
+    debugPrint('->headers: ${options.headers}');
+    debugPrint('->body: ${options.data}');
     return options;
   }
 
   @override
   Future<dynamic> onError(DioError err) async{
-    debugPrint('Error');
-    debugPrint('message: ${err.message}');
-    debugPrint('body: ${err.response.data}');
-    debugPrint('statusCode: ${err.response.statusCode}');
+    debugPrint('<-Error');
+    debugPrint('<-message: ${err.message}');
+    debugPrint('<-body: ${err.response.data}');
+    debugPrint('<-statusCode: ${err.response.statusCode}');
     return err;
   }
 
   @override
   Future<dynamic> onResponse(Response response) async {
-    debugPrint('Response');
-    debugPrint('headers: ${response.headers}');
-    debugPrint('body: ${response.data}');
-    debugPrint('statusCode: ${response.statusCode}');
+    debugPrint('<-Response');
+    debugPrint('<-headers: ${response.headers}');
+    debugPrint('<-body: ${response.data}');
+    debugPrint('<-statusCode: ${response.statusCode}');
     return response;
   }
 }
