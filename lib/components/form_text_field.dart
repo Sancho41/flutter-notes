36c14f2aeedd01af558a72a555b3d51f68263555
Gabriel Sancho <gabriel.sancho13@gmail.com>
2020-07-19 05:56:08 -0300

    feat: Abstracted FormTextField

---
+++ lib/components/form_text_field.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class FormTextField extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final bool isPassword;
+  final bool enabled;
+  final bool actionSubmit;
+  final TextEditingController controller;
+
+  FormTextField({
+    @required this.label,
+    @required this.controller,
+    this.icon,
+    this.enabled = true,
+    this.actionSubmit = false,
+    this.isPassword = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        obscureText: this.isPassword,
+        decoration: InputDecoration(
+          prefixIcon: Icon(this.icon),
+          labelText: this.label,
+        ),
+        controller: this.controller,
+        autocorrect: false,
+        textInputAction:
+            this.actionSubmit ? TextInputAction.done : TextInputAction.next,
+        onSubmitted: (_) => FocusScope.of(context).nextFocus(),
+        enabled: this.enabled,
+        inputFormatters: [
+          BlacklistingTextInputFormatter(RegExp("[\\t]")),
+        ],
+      ),
+    );
+  }
+}
