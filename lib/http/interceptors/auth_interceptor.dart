67c1854169afda8741c7595092c8f005f202c7c8
Gabriel Sancho <gabriel.sancho13@gmail.com>
2020-07-20 03:30:13 -0300

    refact: Token adjust

--- lib/http/interceptors/auth_interceptor.dart
+++ lib/http/interceptors/auth_interceptor.dart
@@ -6,14 +6,14 @@ import 'package:shared_preferences/shared_preferences.dart';
 class AuthInterceptor extends Interceptor{
   @override
   Future<dynamic> onRequest(RequestOptions options) async {
-    if (options.headers.containsKey('requiresToken')) {
-      options.headers.remove('requiresToken');
+    if (options.headers.containsKey('requirestoken')) {
+      options.headers.remove('requirestoken');
 
       SharedPreferences prefs = await SharedPreferences.getInstance();
       String token = prefs.getString('token');
 
       options.headers.addAll({
-        'Authorization': 'Bearer: $token'
+        'Authorization': 'Bearer $token'
       });
     }
 
